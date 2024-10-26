7d73e9f535da6cf319bd03638f76368bd8547405
Neema deshwal <neemadeshwal.py@gmail.com>
2024-10-26 13:08:01 +0530

    create sign up page

---
+++ frontend/components/signup/createAccount.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const CreateAccount = () => {
+  return (
+    <div>
+      <button className="text-white hover:bg-[#1c8bd5] rounded-full py-2 w-full x-bgcolor font-[600]">
+        Create account
+      </button>
+    </div>
+  );
+};
+
+export default CreateAccount;
